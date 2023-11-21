0f6744566f992a6850aff60c0f3a4a1a7c810780
KhoaPham12092002 <105060798+KhoaPham12092002@users.noreply.github.com>
2023-11-21 20:41:00 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Ngại lắm cơ",
+    introTitle: 'Chào Hân nhóa. Web này được làm dành tặng riêng Hân đóa,',
+    introDesc: ` Hình như tui lỡ nốc nhiều nụ cười Hân quá. Lỡ bị say mê mãn tính Hân rồi, Toi mất thoi, phải làm sao phải làm sao cho tim thôi nhớ mong`,
+    btnIntro: 'Giúp tui nhaaaaa !!!!',
+    title: 'Phải chăng vì nụ cười ấy mà mất ngủ kinh niên 🥰',
+    desc: 'Phải chăng vì nụ cười ấy mà ngẫn ngơ thẩn thờ ',
+    btnYes: 'Thích lắm lun á <33',
+    btnNo: 'Không nha bạn thui à :3',
+    question: 'Trên thế giới hơn tận 7 tỉ người cơ mà sao bạn lại thích mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Tại vì bạn cute. Tớ yêu, yêu, yêu cậu lắm cơ !!!!',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều lắm !!!. Mắc cỡ ghê lun á 😘😘',
+    messDesc: 'Hì mình thử code mới lụm thôi chứ thật ra mình thích bạn thiệt ó !!!!!!!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/t/100007636266350' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
